236c61372ca1dc2db8a226eca5f205e21d83110c
qauser <qauser@Johns-MacBook-Pro.local>
2019-05-22 10:53:46 -0400

    for publish

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
-// $(function(){
-//     var logo = $("#logo"),
-//     logoSource = logo.attr("src");
+$(function(){
 
-//     logo.mouseenter(function(){    
-//        var whiteLogoSource = logo.attr("src", "./images/lakefrontLogoWhite.svg");    
-//        logoSource = whiteLogoSource;   
-//     });
+    var logo = $("#logo"),
+    logoSource = logo.attr("src");
 
-//     logo.mouseout(function(){
-//         logoSource = logo.attr("src", "./images/lakefrontLogo.svg");
-//     });
-// });
+    // logo.mouseenter(function(){    
+    //    var whiteLogoSource = logo.attr("src", "./images/lakefrontLogoWhite.svg");    
+    //    logoSource = whiteLogoSource;   
+    // });
+
+    // logo.mouseout(function(){
+    //     logoSource = logo.attr("src", "./images/lakefrontLogo.svg");
+    // });
+    console.log("doc widf");
+
+    console.log('document width ', $(document).width());
+
+    if ($(document).width() <= 1544){
+        logoSource = logo.attr("src", "./images/lakefrontLogo.svg");
+    }
+});
